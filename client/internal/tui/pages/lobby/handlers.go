d7bbd2efc942c240f3d48027b85c51a81e953ed7
sceredi <ceredi.simone.iti@gmail.com>
2026-04-16 17:43:55 +0200

    feat: add player selection

--- client/internal/tui/pages/lobby/handlers.go
+++ client/internal/tui/pages/lobby/handlers.go
@@ -2,8 +2,15 @@ package lobby
 
 import "log"
 
-// --- Message handlers ---
 func handleReadyCmd(m *Model) {
 	m.player.IsReady = !m.player.IsReady
 	log.Printf("Setting ready to %t\n", m.player.IsReady)
+}
+
+func handlePlayerSelect(selectedPlayer int, m *Model) {
+	if selectedPlayer < 0 {
+		m.selectedPlayer = len(m.lobby.Players) - 1
+	} else {
+		m.selectedPlayer = selectedPlayer % len(m.lobby.Players)
+	}
 }
