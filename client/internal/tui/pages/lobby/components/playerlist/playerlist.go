35623be6ffa1a957447f7c50b074d827ad5c0c66
sceredi <ceredi.simone.iti@gmail.com>
2026-04-16 17:32:53 +0200

    feat: create player list component

--- client/internal/tui/pages/lobby/components/playerlist/playerlist.go
+++ client/internal/tui/pages/lobby/components/playerlist/playerlist.go
@@ -1,11 +1,37 @@
 package playerlist
 
-import "github.com/sceredi/co-type/common/domain"
+import (
+	"charm.land/lipgloss/v2"
+	"charm.land/lipgloss/v2/table"
+	"github.com/sceredi/co-type/client/internal/tui/styles"
+	"github.com/sceredi/co-type/common/domain"
+)
 
-type Model struct {
-	lobby domain.Lobby
-}
-
-func New(lobby domain.Lobby) Model {
-	return Model{lobby: lobby}
+func Render(selectedPlayer int, lobby *domain.Lobby) string {
+	rows := make([][]string, len(lobby.Players))
+	for i, player := range lobby.Players {
+		pointer := " "
+		if i == selectedPlayer {
+			pointer = "→"
+		}
+		rows[i] = append(rows[i], pointer)
+		name := player.Name
+		if player.Name == lobby.Host.Name {
+			name = styles.NewLabelBold(player.Name)
+		}
+		rows[i] = append(rows[i], name)
+		ready := " "
+		if player.IsReady {
+			ready = "r"
+		}
+		rows[i] = append(rows[i], ready)
+	}
+	t := table.New().
+		Border(lipgloss.NormalBorder()).
+		BorderColumn(false).
+		StyleFunc(func(row, col int) lipgloss.Style {
+			return lipgloss.NewStyle().Padding(1)
+		}).
+		Rows(rows...)
+	return t.Render()
 }
