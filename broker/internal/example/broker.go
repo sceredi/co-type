e3aebe53ac3abf917910a786a6e44d850d9f38d6
sceredi <ceredi.simone.iti@gmail.com>
2026-04-21 09:52:05 +0200

    chore: fix linter errors

--- broker/internal/example/broker.go
+++ broker/internal/example/broker.go
@@ -1,3 +1,5 @@
+// Package example is just an example package.
 package example
 
+// Name is just a variable.
 var Name = "Broker"
--- common/domain/player.go
+++ common/domain/player.go
@@ -1,3 +1,4 @@
+// Package domain contains the domain models for the co-type game.
 package domain
 
 // Player represents a player in the lobby.
@@ -9,6 +10,7 @@ type Player struct {
 	CanDelete         bool
 }
 
+// NewPlayer creates a new player with the given name and default values for other fields.
 func NewPlayer(name string) Player {
 	return Player{Name: name, IsReady: false}
 }
