6d1523d80de9e84a1ec5e57aedd750fa57ad7ff8
sceredi <ceredi.simone.iti@gmail.com>
2026-04-29 10:03:58 +0200

    chore: group config utils

--- broker/internal/config/config.go
+++ broker/internal/config/config.go
@@ -7,23 +7,14 @@ import (
 	"net"
 	"time"
 
-	"github.com/joho/godotenv"
 	handler "github.com/sceredi/co-type/broker/internal/grpc"
 	"github.com/sceredi/co-type/broker/internal/repository/memory"
 	"github.com/sceredi/co-type/broker/internal/service"
-	"github.com/sceredi/co-type/common/logger"
 	"github.com/sceredi/co-type/common/proto/control"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/keepalive"
 	"google.golang.org/grpc/reflection"
 )
-
-// Setup initializes the configuration.
-func Setup() error {
-	logger.Setup()
-	err := godotenv.Load()
-	return err
-}
 
 // CreateDiscoveryListener creates and starts the gRPC server for the discovery service. It listens on the port specified in the environment variable "BROKER_PORT" and registers the DiscoveryServiceServer with the gRPC server.
 func CreateDiscoveryListener(addr string, lis net.Listener) error {
