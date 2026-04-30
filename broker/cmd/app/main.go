30dd523727f16e9824d870dc81bb862bb34add6a
sceredi <ceredi.simone.iti@gmail.com>
2026-04-30 11:22:47 +0200

    feat: containerize application

--- broker/cmd/app/main.go
+++ broker/cmd/app/main.go
@@ -4,7 +4,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"log/slog"
 	"net"
 	"os"
 	"strconv"
@@ -14,16 +16,41 @@ import (
 )
 
 func main() {
+	writer := io.MultiWriter(os.Stderr)
+	if os.Getenv("LOCAL") == "true" {
+		f, _ := os.OpenFile("debug.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o600)
+		defer func() {
+			if err := f.Close(); err != nil {
+				log.Printf("failed to close file: %v", err)
+			}
+		}()
+
+		writer = io.MultiWriter(os.Stderr, f)
+	}
+	var level slog.Level
+	if err := level.UnmarshalText([]byte(os.Getenv("LOG_LEVEL"))); err != nil {
+		level = slog.LevelInfo
+	}
+	opts := &slog.HandlerOptions{
+		AddSource: true,
+		Level:     level,
+	}
+
+	logger := slog.New(
+		slog.NewJSONHandler(writer, opts),
+	)
+	slog.SetDefault(logger)
+
 	cfg_utils.Setup()
-	port, err := strconv.Atoi(os.Getenv("BROKER_PORT"))
+	controlPort, err := strconv.Atoi(os.Getenv("CONTROL_PORT"))
 	if err != nil {
 		log.Fatalf("Invalid port number: %v", err)
 	}
-	addr := fmt.Sprintf(":%d", port)
+	addr := fmt.Sprintf(":%d", controlPort)
 	lc := net.ListenConfig{}
 	lis, err := lc.Listen(context.Background(), "tcp", addr)
 	if err != nil {
-		log.Fatalf("Failed to listen on port %d: %v", port, err)
+		log.Fatalf("Failed to listen on port %d: %v", controlPort, err)
 	}
 	err = config.CreateDiscoveryListener(addr, lis)
 	if err != nil {
--- common/config/config.go
+++ common/config/config.go
@@ -5,12 +5,10 @@ import (
 	"log/slog"
 
 	"github.com/joho/godotenv"
-	"github.com/sceredi/co-type/common/logger"
 )
 
 // Setup initializes the configuration.
 func Setup() {
-	logger.Setup()
 	err := godotenv.Load()
 	if err != nil {
 		slog.Debug("No .env file found, using environment variables")
